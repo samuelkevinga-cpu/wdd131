79d73dd3f10fbb718904e029ac1bc7634fdb707e
samuelkevinga-cpu <samuelkevinga@gmail.com>
2026-03-12 08:51:27 -0600

    The completed (i hope so) W02 Assignment: Picture album

--- scripts/temples.js
+++ scripts/temples.js
@@ -8,3 +8,17 @@ const lastModifiedPara = document.getElementById("lastModified");
 if (lastModifiedPara) {
     lastModifiedPara.textContent = "Last Modified: " + document.lastModified;
 }
+
+// Hamburger menu toggle
+const hamburguerBtn = document.getElementById("hamburger-btn");
+const navUl = document.querySelector("nav ul");
+hamburguerBtn.addEventListener("click", () => {
+    //Toggle the "open" class on the body element
+    navUl.classList.toggle("open");
+    //Also check if the menu is open or closed, and change the button text to ✕ when open and back to ☰ when closed
+    if (navUl.classList.contains("open"))   {
+        hamburguerBtn.textContent = "✕";
+    } else {
+        hamburguerBtn.textContent = "☰";
+    }
+});
